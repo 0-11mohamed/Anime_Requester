74c4845e34719563e708223f7659e438e6b64cb3
woipy feur apagnan <etupass@etu.unicaen.fr>
2025-09-30 15:54:17 +0200

    feat(card): ajout template et cartes fonctionelles enfonction des données d'un json

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -3,15 +3,29 @@ bg.backgroundImage = "url('assets/background_2.png')";
 bg.backgroundRepeat = "no-repeat";
 bg.backgroundSize = "cover";*/
 
-const ulElement = document.getElementById("anime-list");
+const container = document.getElementById("cardZone");
+const template = document.getElementById("cardTemplate");
 
 fetch('./test.json')
   .then(response => response.json())
   .then(data => {
-    for (const anime of data.data) {
-      const liElement = document.createElement("li");
-      liElement.innerText = anime.title;
-      ulElement.appendChild(liElement);
+    for (const anime of data.data.slice(0, 10)) {
+      const clone = template.content.cloneNode(true);
+
+      clone.querySelector("#cardTitle").textContent = anime.title;
+      clone.querySelector("#cardSynopsis").textContent = anime.synopsis;
+      clone.querySelector("#cardEpisodes").textContent = anime.episodes;
+      clone.querySelector("#cardImg").src = anime.image;
+      if(anime.hasRanking == true){
+        clone.querySelector("#cardClass").textContent = anime.ranking;
+      } else {
+        clone.querySelector("#cardClass").textContent = "No ranking";
+      }
+
+      //clone.querySelector("#cardGenre").textContent = anime.genres;
+      
+      container.appendChild(clone);
+
     }
   })
   .catch(error => {
