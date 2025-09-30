1e6cdf18adb4dc6bc3c7ba9e5a3acb578ab046e9
lylian <montals.lylian2@gmail.com>
2025-09-30 15:51:11 +0200

    début des requêtes

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,7 +1,4 @@
-/*let bg = document.body.style;
-bg.backgroundImage = "url('assets/background_2.png')";
-bg.backgroundRepeat = "no-repeat";
-bg.backgroundSize = "cover";*/
+
 
 const ulElement = document.getElementById("anime-list");
 
