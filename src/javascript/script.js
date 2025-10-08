592b8083e536a21a86e51117cf07dfa9bc6d75bb
woipy feur apagnan <etupass@etu.unicaen.fr>
2025-10-08 13:49:03 +0200

    commit test

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -19,42 +19,10 @@ async function fetchAnime() {
     //editURL();
     const response = await fetch(url, options);
     const result = await response.json();
-    
-    fs.writeFile("test.json", JSON.stringify(result, null, 2), (error) => {
-      if (error) {
-        console.error("Erreur d ecriture :", error);
-        throw error;
-      }
-      console.log("Donnees sauvegardees dans test.json");
-    });
+
+    let jsonData = JSON.parse
 
   } catch (error) {
-const container = document.getElementById("cardZone");
-const template = document.getElementById("cardTemplate");
-
-fetch('./test.json')
-  .then(response => response.json())
-  .then(data => {
-    for (const anime of data.data.slice(0, 10)) {
-      const clone = template.content.cloneNode(true);
-
-      clone.querySelector("#cardTitle").textContent = anime.title;
-      clone.querySelector("#cardSynopsis").textContent = anime.synopsis;
-      clone.querySelector("#cardEpisodes").textContent = anime.episodes;
-      clone.querySelector("#cardImg").src = anime.image;
-      if(anime.hasRanking == true){
-        clone.querySelector("#cardClass").textContent = anime.ranking;
-      } else {
-        clone.querySelector("#cardClass").textContent = "No ranking";
-      }
-
-      //clone.querySelector("#cardGenre").textContent = anime.genres;
-      
-      container.appendChild(clone);
-
-    }
-  })
-  .catch(error => {
     console.error("Erreur lors du fetch :", error);
   }
 }
