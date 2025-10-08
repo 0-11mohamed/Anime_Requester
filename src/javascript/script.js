483904c20863a4e25a3756e808791ae5e348fb57
woipy feur apagnan <etupass@etu.unicaen.fr>
2025-10-08 13:41:26 +0200

    merge(crates)

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,8 +1,3 @@
-/*let bg = document.body.style;
-bg.backgroundImage = "url('assets/background_2.png')";
-bg.backgroundRepeat = "no-repeat";
-bg.backgroundSize = "cover";*/
-
 const container = document.getElementById("cardZone");
 const template = document.getElementById("cardTemplate");
 
