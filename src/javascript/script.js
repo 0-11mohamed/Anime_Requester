afa7fc720bc8c0b10913550ed9325e024dad2c4c
lylian <montals.lylian2@gmail.com>
2025-10-08 13:39:51 +0200

    edit: requête avec l'API

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,16 +1,36 @@
+const fs = require("fs");
 
+const url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=vinland';
 
-const ulElement = document.getElementById("anime-list");
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '75b6251b0fmsh68cc306febf1b84p1a443fjsnff7449ddea1a',
+		'x-rapidapi-host': 'anime-db.p.rapidapi.com'
+	}
+};
 
-fetch('./test.json')
-  .then(response => response.json())
-  .then(data => {
-    for (const anime of data.data) {
-      const liElement = document.createElement("li");
-      liElement.innerText = anime.title;
-      ulElement.appendChild(liElement);
-    }
-  })
-  .catch(error => {
+function editURL(){
+
+}
+
+async function fetchAnime() {
+  try {
+    //editURL();
+    const response = await fetch(url, options);
+    const result = await response.json();
+    
+    fs.writeFile("test.json", JSON.stringify(result, null, 2), (error) => {
+      if (error) {
+        console.error("Erreur d ecriture :", error);
+        throw error;
+      }
+      console.log("Donnees sauvegardees dans test.json");
+    });
+
+  } catch (error) {
     console.error("Erreur lors du fetch :", error);
-  });
+  }
+}
+
+fetchAnime();
