ff7cbeea4ba72fb103022ad3052650631665b318
lylian <montals.lylian2@gmail.com>
2025-10-08 14:56:23 +0200

    gestion de l'url pour requete

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,6 +1,25 @@
-const fs = require("fs");
+let url = 'url'; 
 
-const url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=vinland';
+document.addEventListener('DOMContentLoaded', () => {
+  const filter = document.getElementById('filter');
+  const query = document.getElementById('query');
+  const delButton = document.getElementById('delButton');
+  const searchButton = document.getElementById('searchButton');
+
+  delButton.addEventListener('click', () => {
+      query.value = ""; 
+  });
+
+  searchButton.addEventListener('click', (event) => {
+    const filterValue = filter.value;
+    const queryText = query.value;
+
+    console.log("Filtre :", filterValue);
+    console.log("Texte :", queryText);
+
+    fetchAnime(filterValue, queryText);
+  });
+});
 
 const options = {
 	method: 'GET',
@@ -10,21 +29,26 @@ const options = {
 	}
 };
 
-function editURL(){
-
-}
-
-async function fetchAnime() {
-  try {
-    //editURL();
-    const response = await fetch(url, options);
-    const result = await response.json();
-
-    let jsonData = JSON.parse
-
-  } catch (error) {
-    console.error("Erreur lors du fetch :", error);
+function editURL(filterValue, queryText) {
+  switch(filterValue) {
+      case 'title':
+          url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=' + queryText.trim() + '&sortBy=ranking&sortOrder=asc';
+          break;
+      case 'id':
+          url = 'https://anime-db.p.rapidapi.com/anime/by-id/' + queryText.trim();
+          break;
+      case 'rank':
+          url = 'https://anime-db.p.rapidapi.com/anime/by-ranking/' + queryText.trim();
+          break;
+      default:
+          url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10';
   }
 }
 
-fetchAnime();
+
+async function fetchAnime(filterValue, queryText) {
+  editURL(filterValue, queryText);
+  const response = await fetch(url, options);
+  const result = await response.json();
+  console.log(result);
+}
